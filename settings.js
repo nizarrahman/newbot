236d90bb5ae231c47395639cf0f09be343ce1f76
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-01-03 03:21:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,26 +15,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6281386945391'] //ur owner number
+global.ownername = "Nizar" //ur owner name
+global.ytname = "-" //ur yt chanel name
+global.socialm = "Insta: https://instagram.com/nizarrrahman.f_" //ur github or insta name
+global.location = "Cianjur, Jawa Barat, Indonesia" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
+global.ownernomer = "6281386945391" //ur number
+global.premium = ['6281386945391'] //ur premium number
+global.botname = '𝘽𝙤𝙩 𝙉𝙖𝙞 𝙎𝙩𝙤𝙧𝙚' //ur bot name
+global.linkz = "https://chat.whatsapp.com/BWHiCcMOZG39gYt1HCc2ue" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "𝘽𝙤𝙩 𝙉𝙖𝙞 𝙎𝙩𝙤𝙧𝙚\n\n\nA whatsapp bot developed by\nNizar Rahman\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +6281386945391" //ur sticker watermark author
+global.wm = "𝘽𝙤𝙩 𝙉𝙖𝙞 𝙎𝙩𝙤𝙧𝙚." //ur watermark
 
 // Other
-global.sessionName = 'session'
+global.sessionName = 'Nizar'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 
